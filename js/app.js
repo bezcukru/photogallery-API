c56bd08313af0734db69b64eaeb351f6c68ce684
Agata Kuska <kawabezcukru@onet.eu>
2020-06-22 16:22:09 +0200

    Add listBreeds and getRandomImage functions and test them

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
-import '../sass/style.scss';
+//import '../sass/style.scss';
 
 
 function listBreeds() {
-    fetch('https://dog.ceo/api/breeds/list/all')
+    return fetch('https://dog.ceo/api/breeds/list/all')
         .then(resp => resp.json())
         .then(data => {
-            console.log(data.message);
-        })
+            return data.message;
+    })
+        .catch(err => console.log('Coś poszło nie tak! ' + err));
 }
 
 listBreeds();
+
+function getRandomImage() {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(data => data.message);
+}
+
+const imgTag = document.querySelector('img');
+
+getRandomImage().then(img => imgTag.setAttribute('src', img))
