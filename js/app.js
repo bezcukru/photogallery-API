ab0bd054f49311cd43aa04d01e977a87793a0553
Agata Kuska <kawabezcukru@onet.eu>
2020-06-23 16:27:14 +0200

    add showImageWhenReady function

--- js/app.js
+++ js/app.js
@@ -12,71 +12,74 @@ class Dog {
         this.init();
     }
 
-showLoading() {
-    this.spinnerEl.classList.add('spinner--visible');
-}
-
-hideLoading() {
-    this.spinnerEl.classList.remove('spinner--visible');
-}
-
-showAllBreeds() {
-    return fetch(`${this.apiUrl}/breeds/list/all`)
-        .then(resp => resp.json())
-        .then(data => {
-            return data.message;
-    })
-        .catch(err => console.log('Coś poszło nie tak! ' + err));
-}
-
-getRandomImage() {
-    return fetch(`${this.apiUrl}/breeds/image/random`)
-        .then(resp => resp.json())
-        .then(data => data.message);
-}
-
-getRandomImageByBreed(breed) {
-    return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
-        .then(resp => 
-            resp.json())
-        .then(data => data.message);
-
-}
-
-init() {
-    this.showLoading();
-    this.getRandomImage()
-        .then(src => {
-            this.imgEl.setAttribute('src', src);
-            this.backgroundEl.style.backgroundImage = `url("${src}")`;
-            this.hideLoading();
-        }),
-    this.showAllBreeds()
-        .then(breeds => {
-            for (const breed in breeds) {
-                console.log(breed, breeds[breed]);
-                if (breeds[breed].length === 0) {
-                    this.addBreed(breed);
-                } else {
-                    for (const subBreed of breeds[breed]) {
-                        this.addBreed(breed, subBreed)
-                    }
-                }
-                
-            }
-        })
-    
+    showLoading() {
+        this.spinnerEl.classList.add('spinner--visible');
     }
+
+    hideLoading() {
+        this.spinnerEl.classList.remove('spinner--visible');
+    }
+
+    showAllBreeds() {
+        return fetch(`${this.apiUrl}/breeds/list/all`)
+            .then(resp => resp.json())
+            .then(data => {
+                return data.message;
+            })
+            .catch(err => console.log('Coś poszło nie tak! ' + err));
+    }
+
+    getRandomImage() {
+        return fetch(`${this.apiUrl}/breeds/image/random`)
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+
+    getRandomImageByBreed(breed) {
+        return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+            .then(resp =>
+                resp.json())
+            .then(data => data.message);
+
+    }
+
+    init() {
+        this.showLoading();
+        this.getRandomImage()
+            .then(image => this.showImageWhenReady(image));
+        this.showAllBreeds()
+            .then(breeds => {
+                for (const breed in breeds) {
+                    console.log(breed, breeds[breed]);
+                    if (breeds[breed].length === 0) {
+                        this.addBreed(breed);
+                    } else {
+                        for (const subBreed of breeds[breed]) {
+                            this.addBreed(breed, subBreed)
+                        }
+                    }
+
+                }
+            })
+
+    }
+
+    showImageWhenReady(image) {
+        this.imgEl.setAttribute('src', image);
+        this.backgroundEl.style.backgroundImage = `url("${image}")`;
+        this.hideLoading();
+    }
+
     addBreed(breed, subBreed) {
         let name;
         let type;
 
         if (typeof subBreed === 'undefined') {
-            name = breed; 
+            name = breed;
             type = breed;
         } else {
-            name = `${subBreed} ${breed}`; 
-            type = `${breed}/${subBreed}`; 
+            name = `${subBreed} ${breed}`;
+            type = `${breed}/${subBreed}`;
         }
 
         const tile = document.createElement('div');
@@ -86,19 +89,18 @@ init() {
         tileContent.classList.add('tiles__tile--content');
         tileContent.innerText = name;
         tileContent.addEventListener('click', () => {
+            window.scrollTo(0, 0);
             this.showLoading();
             this.getRandomImageByBreed(type)
-             .then(src => {
-                this.imgEl.setAttribute('src', src);
-                this.backgroundEl.style.backgroundImage = `url("${src}")`;
-                this.hideLoading();
-                this.descriptionEl.textContent = `You are now looking at a beautiful ${name}!`;
-            });
-    });
-    tile.appendChild(tileContent);
-    this.tilesEl.appendChild(tile);
-}
-    
+                .then(image => {
+                    this.showImageWhenReady(image);
+                    this.descriptionEl.textContent = `You are now looking at a gorgeous ${name}!`;
+                });
+        });
+        tile.appendChild(tileContent);
+        this.tilesEl.appendChild(tile);
+    }
+
 };
 
 document.addEventListener('DOMContentLoaded', () => {
